72be9d879c3bca200c384510a3684aeee16944af
Cenk Gokturk <cenkgokturk@Cenks-MacBook-Pro-2.local>
2022-02-03 23:15:13 +0300

    Login/Register pages are now fully functional

--- ideapp-mobile/ContentView.swift
+++ ideapp-mobile/ContentView.swift
@@ -11,88 +11,106 @@ import BCrypt
 import Foundation
 import Combine
 
+struct HomeScreen: View {
+    
+    @Binding var showHomeScreen: Bool
+    
+    var body: some View {
+        Text("Home Screen")
+    }
+}
+
+//MARK: - Login
 struct Login: View {
+    
+    
+    
     @State var email:String = ""
     @State var password:String = ""
     
     @State private var type = 0
     
     @Binding var showLogin: Bool
+    @Binding var showHomeScreen: Bool
     @State var manager = DataPost()
     
     var body: some View {
-  
-
+        
+        
         VStack(spacing: 10) {
             
             Spacer()
             
             Text("Login")
-              .bold()
-              .font(.title)
+                .bold()
+                .font(.title)
             
             Picker("Your status?", selection: $type) {
-                            Text("Student").tag(0)
-                            Text("Instructor").tag(1)
-                        }
-                        .pickerStyle(.segmented)
-                        .padding()
+                Text("Student").tag(0)
+                Text("Instructor").tag(1)
+            }
+            .pickerStyle(.segmented)
+            .padding()
             
-
+            
             
             TextField("Mail",text: $email)
-              .padding()
-              .background(Color(.systemGray6))
-              .cornerRadius(5.0)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(5.0)
             SecureField("Password",text: $password)
-              .padding()
-              .background(Color(.systemGray6))
-              .cornerRadius(5.0)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(5.0)
             
             Button(action: {
-                var username = email.components(separatedBy: "@")[0]
+                //var username = email.components(separatedBy: "@")[0]
                 
-                do {
-                    let salt = "$2b$12$fDlH7QbAJWGIlledMab6le"
-                    let hashed = try BCrypt.Hash(password, salt: salt)
-                    
-                    var entityToInsert: NSDictionary = ["username":"namesurname", "password": hashed]
-                    var isAuthorized = manager.login(user: entityToInsert, collection: "Instructor")
-                    print("Result is \(isAuthorized)")
+                var currentCollection: String = "students"
+                
+                if type == 1 {
+                    currentCollection = "Instructor"
                 }
-                catch {
-                    print("An error occured: \(error)")
+                
+                var entityToInsert: NSDictionary = ["email":email, "password": password]
+                var isAuthorized = manager.login(user: entityToInsert, collection: currentCollection)
+                
+                print("Result of isAuthorized is \(isAuthorized)")
+                
+                if isAuthorized {
+                    self.showHomeScreen = true
                 }
                 
             }, label: {
-              HStack {
-                Spacer()
-                Text("Login")
-                   .foregroundColor(.white)
-                Spacer()
+                HStack {
+                    Spacer()
+                    Text("Login")
+                        .foregroundColor(.white)
+                    Spacer()
                 }.padding()
-                 .background(Color.blue)
-                 .cornerRadius(5.0)
+                    .background(Color.blue)
+                    .cornerRadius(5.0)
             })
             
             Button(action: {
                 self.showLogin = false
             }, label: {
-              HStack {
-                Spacer()
-                Text("Sign up")
-                      .foregroundColor(.blue)
-                Spacer()
+                HStack {
+                    Spacer()
+                    Text("Sign up")
+                        .foregroundColor(.blue)
+                    Spacer()
                 }.padding()
                     .background(Color.white)
-                 .cornerRadius(5.0)
+                    .cornerRadius(5.0)
             })
             
-          Spacer()
+            Spacer()
         }.padding()
     }
 }
 
+//MARK: - Register
 struct Register: View {
     @State var name:String = ""
     @State var sirname:String = ""
@@ -101,50 +119,52 @@ struct Register: View {
     @State var password:String = ""
     
     @State private var type = 0
+    
     @Binding var showLogin: Bool
+    @Binding var showHomeScreen: Bool
     
     @State var manager = DataPost()
     
     var body: some View {
-  
+        
         VStack(spacing: 10) {
             
             Spacer()
             
             Text("Register")
-              .bold()
-              .font(.title)
+                .bold()
+                .font(.title)
             
             Picker("Your status?", selection: $type) {
-                            Text("Student").tag(0)
-                            Text("Instructor").tag(1)
-                        }
-                        .pickerStyle(.segmented)
-                        .padding()
+                Text("Student").tag(0)
+                Text("Instructor").tag(1)
+            }
+            .pickerStyle(.segmented)
+            .padding()
             
             VStack{
                 TextField("Name",text: $name)
-                  .padding()
-                  .background(Color(.systemGray6))
-                  .cornerRadius(5.0)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(5.0)
                 TextField("Surname",text: $sirname)
-                  .padding()
-                  .background(Color(.systemGray6))
-                  .cornerRadius(5.0)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(5.0)
                 TextField("Email",text: $email)
-                  .padding()
-                  .background(Color(.systemGray6))
-                  .cornerRadius(5.0)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(5.0)
                 TextField("Password",text: $password)
-                  .padding()
-                  .background(Color(.systemGray6))
-                  .cornerRadius(5.0)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(5.0)
                 
                 if type == 0 {
                     TextField("Student ID",text: $studentId)
-                      .padding()
-                      .background(Color(.systemGray6))
-                      .cornerRadius(5.0)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(5.0)
                 }
             }
             
@@ -160,9 +180,15 @@ struct Register: View {
                     if type == 0 { // if student
                         //var entityToInsert: NSDictionary = ["name":name, "sirname": sirname, "email": email, "password": password, "student-id": studentId]
                         //manager.registerInstructor(instructor: entityToInsert)
+                        var entityToInsert: NSDictionary = ["name":name, "sirname": sirname, "student-id": studentId ,"email": email, "password": hashed]
+                        manager.signup(user: entityToInsert, collection: "students")
+                        
+                        self.showHomeScreen = true
                     } else {
-                        var entityToInsert: NSDictionary = ["name":name, "sirname": sirname, "email": email, "username": username,"password": hashed]
+                        var entityToInsert: NSDictionary = ["name":name, "sirname": sirname, "email": email, "password": hashed]
                         manager.signup(user: entityToInsert, collection: "Instructor")
+                        
+                        self.showHomeScreen = true
                     }
                 }
                 catch {
@@ -173,30 +199,30 @@ struct Register: View {
                 
                 
             }, label: {
-              HStack {
-                Spacer()
-                Text("Register")
-                   .foregroundColor(.white)
-                Spacer()
+                HStack {
+                    Spacer()
+                    Text("Register")
+                        .foregroundColor(.white)
+                    Spacer()
                 }.padding()
-                 .background(Color.blue)
-                 .cornerRadius(5.0)
+                    .background(Color.blue)
+                    .cornerRadius(5.0)
             })
             
             Button(action: {
                 self.showLogin = true
             }, label: {
-              HStack {
-                Spacer()
-                Text("Login")
-                      .foregroundColor(.blue)
-                Spacer()
+                HStack {
+                    Spacer()
+                    Text("Login")
+                        .foregroundColor(.blue)
+                    Spacer()
                 }.padding()
                     .background(Color.white)
-                 .cornerRadius(5.0)
+                    .cornerRadius(5.0)
             })
             
-          Spacer()
+            Spacer()
         }.padding()
     }
 }
@@ -204,22 +230,31 @@ struct Register: View {
 struct ContentView: View {
     
     @State var showLogin = true
+    @State var showHomeScreen = false
+    
     @State var manager = DataPost()
     
     init() {
-
     }
     
-
+    
     var body: some View {
         return Group {
-                    if showLogin {
-                        Login(showLogin: $showLogin)
-                    }
-                    else {
-                        Register(showLogin: $showLogin)
-                    }
+                
+            
+
+                if showLogin == true && showHomeScreen == false {
+                    Login(showLogin: $showLogin, showHomeScreen: $showHomeScreen)
                 }
+                else if showLogin == false && showHomeScreen == false{
+                    Register(showLogin: $showLogin, showHomeScreen: $showHomeScreen)
+                }else{
+                    HomeScreen(showHomeScreen: $showHomeScreen)
+                }
+
+            
+            
+        }
     }
 }
 
@@ -234,13 +269,17 @@ class DataPost: ObservableObject {
     @Published var receivedResponse: [String: Any] = [:]
     
     var done = false
+    var isAuthorized: Bool = false
+    
+    var loginDone = false
+    var receivedHASHDone = ""
     
     func signup(user: NSDictionary, collection: String){
         
-        let body: [String: Any] = ["collection": "Instructor",
-                                      "database": "ideapp",
-                                      "dataSource": "ProjectCluster",
-                                      "document": user ]
+        let body: [String: Any] = ["collection": collection,
+                                   "database": "ideapp",
+                                   "dataSource": "ProjectCluster",
+                                   "document": user ]
         
         let jsonData = try? JSONSerialization.data(withJSONObject: body)
         
@@ -278,20 +317,19 @@ class DataPost: ObservableObject {
         task.resume()
     }
     
-    // 
+    //
     
     func login(user: NSDictionary, collection: String) -> Bool{
         
-        var isAuthorized: Bool = false
-        
-        
-        let body: [String: Any] = ["collection": "Instructor",
-                                      "database": "ideapp",
-                                      "dataSource": "ProjectCluster",
-                                   "filter": ["username": user["username"] ] ]
+        let body: [String: Any] = ["collection": collection,
+                                   "database": "ideapp",
+                                   "dataSource": "ProjectCluster",
+                                   "filter": ["email": user["email"] ] ]
         
         let jsonData = try? JSONSerialization.data(withJSONObject: body)
         
+        print("-----> user: \(user)")
+        print("-----> body: \(body)")
         print("-----> jsonData: \(jsonData)")
         
         let url = URL(string: "https://data.mongodb-api.com/app/data-rbevh/endpoint/data/beta/action/findOne")!
@@ -320,16 +358,20 @@ class DataPost: ObservableObject {
             if let responseJSON = responseJSON as? [String: Any] {
                 print("-----2> responseJSON: \(responseJSON)")
                 self.receivedResponse = responseJSON
-                self.done = true
+                self.loginDone = true
                 
-                var receivedJSON = responseJSON["document"] as! [String:String]
+                var receivedJSON = responseJSON["document"] as! [String:Any]
+                var receivedHASH: String = receivedJSON["password"] as! String
                 
-                var receivedPassword: String = user["password"] as! String
-                var originalPassword: String = receivedJSON["password"] as! String
+                self.receivedHASHDone = receivedHASH
                 
-                if  receivedPassword == originalPassword {
-                    isAuthorized = true
-                }
+                var givenPassword: String = user["password"] as! String
+                
+                print("givenPassword \(givenPassword) & receivedHASH \(receivedHASH)")
+                
+                self.isAuthorized = BCrypt.Check(givenPassword, hashed: receivedHASH)
+                print("isAuthorized function \(self.isAuthorized)")
+                
             }
             
         }
@@ -337,9 +379,14 @@ class DataPost: ObservableObject {
         task.resume()
         
         repeat {
-            RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
-        } while !done
+            RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.2))
+        } while !loginDone
         
+        while receivedHASHDone == "" {
+            
+        }
+        
+        print("return isAuthorized function \(self.isAuthorized)")
         
         return isAuthorized
     }
