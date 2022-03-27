7e6da9d03f37d0c0303ec3332f7278e19704352f
atahanunl <atahanunl@gmail.com>
2022-03-27 18:55:39 +0300

    Grades page has been added

---
+++ Grades.swift
@@ -0,0 +1,31 @@
+//
+//  Grades.swift
+//  ideapp-mobile
+//
+//  Created by Atahan Ünal on 3/27/22.
+//
+
+import SwiftUI
+
+//MARK: - Show student grades
+struct Grades: View {
+
+    var body: some View {
+        VStack{
+            VStack (alignment: .leading){
+                HStack {
+                    Spacer()
+                    Text("Your grades")
+                        .font(.system(size: 30))
+                        .bold()
+                        .padding(.trailing)
+                    Spacer()
+                }
+            
+            }
+            
+            
+            
+        }
+    }
+}
